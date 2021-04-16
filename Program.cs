b73b98c3aa2cecf820192f42c11f58ca3fa27a7a
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-04-16 08:58:19 +0530

    Added code of UC2_PINCODE_USING_SPECIALCHAR_ALPHABET

--- Program.cs
+++ Program.cs
@@ -2,17 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace REGREXPATTERN
+namespace RegexDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WELCOME TO REGREX PATTERN PROBLEMS ");
-            Console.ReadLine();
+            string pincode = "^[0-9]{3}[]*[0-9]{3}$";
+            string[] pinCodeArr = { "422010", "A42109", "40C820", "145225" };
+            Program p = new Program();
 
+            p.ValidPincode(pinCodeArr, pincode);
+            Console.WriteLine();
+            Console.Read();
+
+        }
+
+        public void ValidPincode(string[] arr, string pincode)
+        {
+            Console.WriteLine("Validation Indian PIN Code");
+            Regex regex = new Regex(pincode);
+            IterateLoop(arr, regex);
+        }
+        public void IterateLoop(string[] arr, Regex regex)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool result = regex.IsMatch(arr[i]);
+                if (result == true)
+                {
+                    Console.WriteLine(arr[i] + "---->" + "Valid");
+                }
+                else
+                {
+                    Console.WriteLine(arr[i] + "--->" + "Invalid");
+                }
+            }
         }
     }
 }
