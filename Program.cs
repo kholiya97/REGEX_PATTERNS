cfe0fb72a6c1dbe4bc1d4313d7350ea9ddbc7008
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-04-16 12:40:29 +0530

    Added file of UC3_ALPHABET_AT_THE_END

--- Program.cs
+++ Program.cs
@@ -10,9 +10,36 @@ namespace REGREXPATTERN
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WELCOME TO REGREX PATTERN PROBLEMS ");
-            Console.ReadLine();
+            string pincode = "^[0-9]{3}[]*[0-9]{3}$";
+            string[] pinCodeArr = { "422010", "482109", "40C82K", "145c22" };
+            Program p = new Program();
 
+            p.ValidPincode(pinCodeArr, pincode);
+            Console.WriteLine();
+            Console.Read();
+
+        }
+
+        public void ValidPincode(string[] arr, string pincode)
+        {
+            Console.WriteLine("Validation Indian PIN Code");
+            REGREX regex = new REGREX(pincode);
+            IterateLoop(arr, regex);
+        }
+        public void IterateLoop(string[] arr, Regex regex)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool result = regex.IsMatch(arr[i]);
+                if (result == true)
+                {
+                    Console.WriteLine(arr[i] + "---->" + "Valid");
+                }
+                else
+                {
+                    Console.WriteLine(arr[i] + "--->" + "Invalid");
+                }
+            }
         }
     }
 }
