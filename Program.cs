52a6ded5da09bf5b71ed63b3c5c056ea530d87b3
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-04-16 12:46:20 +0530

    Added File of UC4_VALID_SPACE_FOR_PIN

--- Program.cs
+++ Program.cs
@@ -2,17 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace REGREXPATTERN
+namespace RegexDemo
 {
     class Program
     {
         static void Main(string[] args)
+        //Entery ponit
         {
-            Console.WriteLine("WELCOME TO REGREX PATTERN PROBLEMS ");
-            Console.ReadLine();
+            //string pincode = "^[0-9]{6}$"; //pattern using for alphabets
+            string pincode = "^[0-9]{3}[ ]*[0-9]{3}$"; //pattern
+            string[] pinCodeArr = { "422010", "482109", "145222", "145 222", "422 010" }; //Array
 
+            Program p = new Program(); // create object
+            p.ValidPincode(pinCodeArr, pincode);
+            Console.WriteLine();
+            Console.Read();
+
+        }
+        public void ValidPincode(string[] arr, string pincode)
+        {
+            Console.WriteLine("Validation Indian PIN Code");
+            Regex regex = new Regex(pincode);
+            IterateLoop(arr, regex);  //calling method
+        }
+        public void IterateLoop(string[] arr, Regex regex)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool result = regex.IsMatch(arr[i]);
+                if (result == true)
+                {
+                    Console.WriteLine(arr[i] + "---->" + "Valid");
+                }
+                else
+                {
+                    Console.WriteLine(arr[i] + "--->" + "Invalid");
+                }
+            }
         }
     }
 }
