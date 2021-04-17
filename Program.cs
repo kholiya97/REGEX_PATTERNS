420e29a0ec7775672c3617c1d5b0acd5fa816e06
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-04-18 00:37:54 +0530

    Added file of UC9_ENTER_EMAIL_SAMPLE_VALID_OR_INVALID

--- Program.cs
+++ Program.cs
@@ -14,43 +14,47 @@ namespace UserRegistration
 
             Console.WriteLine("-------Welcome To User Registration--------");
             Console.WriteLine();
-            string pattern = "[@#$%^&+=]{1}"; //pattern for exactly 1special char. 
+            string pattern = "^([A-Za-z\\d-_\\+]+)(\\.[A-Za-z\\d-_]+)?@([a-zA-Z\\d]+)\\.([a-zA-Z]{2,4})(\\.[A-Za-z]{2,4})?$"; //pattern for email sample 
+                                                                                                                              // ASSIGING SOME VALID SAMPLES
+            string[] inputs = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com", "abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com",
+                              ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com",
+                                "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au" };
 
             Program p = new Program(); // create object
-            p.ValidPass(pattern);
+            p.ValidPass(inputs, pattern);
             Console.WriteLine();
             Console.Read();
         }
 
-        public void ValidPass(string pattern) //method to validate string
+        public void ValidPass(string[] arr, string pattern) //method to validate string
         {
 
-            Console.WriteLine("Validation Of The Password");
+            Console.WriteLine("Validation Of The email");
             Regex regex = new Regex(pattern);
-            IterateLoop(regex);  //calling method
+            IterateLoop(arr, regex);  //calling method
         }
 
-        public void IterateLoop(Regex regex)
+        public void IterateLoop(string[] arr, Regex regex)
         {
-            int i = 0; //initialize i value
-            while (i != 1)
+            for (int i = 0; i < arr.Length; i++)    //check each name string using for loop 
+
             {
-                Console.WriteLine("Enter Password");
-                string Password = Console.ReadLine();
-                bool result = regex.IsMatch(Password);  //call the IsMatch metod to determine whether a match is present
+
+                bool result = regex.IsMatch(arr[i]);  //call the IsMatch metod to determine whether a match is present
 
                 if (result == true)  ////check result is true or not using if and hence using bool
                 {
-                    Console.WriteLine("Valid password");
-                    i = 1;
+                    Console.WriteLine(arr[i] + "---->" + "Valid email");
+
                 }
                 else
                 {
-                    Console.WriteLine("Enter atlest one special character");
+                    Console.WriteLine(arr[i] + "--->" + "Invalid email");
                 }
 
             }
         }
+
 
     }
 }
