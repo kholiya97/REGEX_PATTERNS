600877da8859f03b59a00168952a31022ebe22bc
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-04-18 00:22:23 +0530

    Added file of UC6_PASSWORD_ATLEAST_1UPPERCASE

--- Program.cs
+++ Program.cs
@@ -2,17 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace REGREXPATTERN
+namespace UserRegistration
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("WELCOME TO REGREX PATTERN PROBLEMS ");
-            Console.ReadLine();
+        {     //Entery point 
 
+            Console.WriteLine("-------Welcome To User Registration--------");
+            Console.WriteLine();
+            string pattern = "^[A-Z]{1}[a-z]{7,}$"; //pattern for password minimum 8 char.
+
+            Program p = new Program(); // create object
+            p.ValidPass(pattern);
+            Console.WriteLine();
+            Console.Read();
         }
+
+        public void ValidPass(string pattern) //method to validate string
+        {
+
+            Console.WriteLine("Validation Of The Password");
+            Regex regex = new Regex(pattern);
+            IterateLoop(regex);  //calling method
+        }
+
+        public void IterateLoop(Regex regex)
+        {
+            int i = 0; //initialize i value
+            while (i != 1)
+            {
+                Console.WriteLine("Enter Password");
+                string Password = Console.ReadLine();
+                bool result = regex.IsMatch(Password);  //call the IsMatch metod to determine whether a match is present
+
+                if (result == true)  ////check result is true or not using if and hence using bool
+                {
+                    Console.WriteLine("Valid password");
+                    i = 1;
+                }
+                else
+                {
+                    Console.WriteLine("Enter minimum eight character and use atlest one Uppercase");
+                }
+
+            }
+        }
+
     }
 }
